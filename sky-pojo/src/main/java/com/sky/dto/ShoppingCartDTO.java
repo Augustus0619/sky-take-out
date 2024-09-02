160de768453c653a3acd3db447afeafd18461dc9
hwy <893032915@qq.com>
2024-09-02 18:44:16 +0800

    新增购物车添加菜品套餐功能

--- sky-pojo/src/main/java/com/sky/dto/ShoppingCartDTO.java
+++ sky-pojo/src/main/java/com/sky/dto/ShoppingCartDTO.java
@@ -6,6 +6,7 @@ import java.io.Serializable;
 @Data
 public class ShoppingCartDTO implements Serializable {
 
+    //dishId和setmealId只能选一个（一次添加“+”,只可能添加一个菜品或是一个套餐，不可能两个都添加）
     private Long dishId;
     private Long setmealId;
     private String dishFlavor;
---
+++ sky-server/src/main/java/com/sky/service/impl/ShoppingCartServiceImpl.java
@@ -0,0 +1,87 @@
+package com.sky.service.impl;
+
+import com.sky.context.BaseContext;
+import com.sky.dto.ShoppingCartDTO;
+import com.sky.entity.Dish;
+import com.sky.entity.Setmeal;
+import com.sky.entity.ShoppingCart;
+import com.sky.mapper.DishMapper;
+import com.sky.mapper.SetmealMapper;
+import com.sky.mapper.ShoppingCartMapper;
+import com.sky.service.ShoppingCartService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Slf4j
+@Service
+public class ShoppingCartServiceImpl implements ShoppingCartService
+{
+
+    @Autowired
+    private ShoppingCartMapper shoppingCartMapper;
+
+    @Autowired
+    private DishMapper dishMapper;
+
+    @Autowired
+    private SetmealMapper setmealMapper;
+
+    /**
+     *
+     * @param shoppingCartDTO
+     */
+    @Override
+    public void addShoppingCart(ShoppingCartDTO shoppingCartDTO)
+    {
+
+        //review:感觉这里ShoppingCart改为ShoppingCartItem可读性较强一些
+        ShoppingCart shoppingCart = new ShoppingCart();
+        BeanUtils.copyProperties(shoppingCartDTO,shoppingCart);
+
+        //设置用户id,只能查询自己购物车的数据
+        shoppingCart.setUserId(BaseContext.getCurrentId());
+
+        //判断当前商品是否在购物车中,如果已经存在，就把数量加1，不存在的话就插入数据，数量为1
+        //shoppingCartDTO中只能有一个套餐或是一个菜品，会出现空，所以需要用动态sql查询
+        List<ShoppingCart> shoppingCartList = shoppingCartMapper.list(shoppingCart);
+
+        //这里只有一条数据但是用List来接收是为了后续查看购物车做准备
+        if(shoppingCartList != null && shoppingCartList.size() == 1)
+        {
+            shoppingCart = shoppingCartList.get(0);
+            shoppingCart.setNumber(shoppingCart.getNumber() + 1);
+            //感觉这里传入shoppingCart而不是Id是为了后续复用，可以拓展出根据其他字段更新
+            shoppingCartMapper.updateNumberById(shoppingCart);
+        }
+        else
+        {
+            //判断插入的数据是菜品还是套餐
+            Long dishId = shoppingCartDTO.getDishId();
+            Long setmealId = shoppingCartDTO.getSetmealId();
+            if(dishId != null)
+            {
+                Dish dish = dishMapper.getById(dishId);
+                shoppingCart.setName(dish.getName());
+                shoppingCart.setImage(dish.getImage());
+                shoppingCart.setAmount(dish.getPrice());
+            }
+            else
+            {
+                Setmeal setmeal = setmealMapper.getById(setmealId);
+                shoppingCart.setName(setmeal.getName());
+                shoppingCart.setImage(setmeal.getImage());
+                shoppingCart.setAmount(setmeal.getPrice());
+            }
+            shoppingCart.setNumber(1);
+            shoppingCart.setCreateTime(LocalDateTime.now());
+            shoppingCartMapper.insert(shoppingCart);
+        }
+
+
+    }
+}
